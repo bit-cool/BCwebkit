26c4080b0176e669cfd23816f9c9b301420f1bb1
cxw2014 <cxw@FreddydeMac-mini.local>
2015-10-22 17:18:36 +0800

    bitcool introduction
    bitcool introduction

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$(".intro").bind("mouseover",function(){
+		$(".img-top").animate({
+			opacity:'0.5',
+			height:'400px'
+		},'slow');
+	});
+	$(".intro").bind("mouseout",function(){
+		$(".img-top").animate({
+			opacity:'0',
+			height:'0px'
+		},'slow');
+	});
+});
