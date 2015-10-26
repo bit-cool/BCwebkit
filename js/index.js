5f8732e87a61d6a4419de10d93a8e7439b899d91
cxw2014 <cxw@FreddydeMac-mini.local>
2015-10-26 15:05:51 +0800

    contact us finished
    contact us finished

--- js/index.js
+++ js/index.js
@@ -1,22 +1,14 @@
 $(document).ready(function(){
 	$(".intro").bind("mouseover",function(){
-		$(".img-top").animate({
+		$(".img-top").css({
 			opacity:'0.5',
 			height:'400px'
-<<<<<<< HEAD
-		},"slow","linear");
-=======
-		},"slow");
->>>>>>> origin/gh-pages
+		});
 	});
 	$(".intro").bind("mouseout",function(){
-		$(".img-top").animate({
+		$(".img-top").css({
 			opacity:'0',
 			height:'0px'
-<<<<<<< HEAD
-		},"slow","linear");
-=======
-		},"slow");
->>>>>>> origin/gh-pages
+		});
 	});
 });
