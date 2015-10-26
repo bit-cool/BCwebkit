6483661e02ce6359269336857c3189bc707b7dab
cxw2014 <cxw@FreddydeMac-mini.local>
2015-10-26 16:09:11 +0800

    production and service finished
    production and service finished

--- js/index.js
+++ js/index.js
@@ -11,4 +11,26 @@ $(document).ready(function(){
 			height:'0px'
 		});
 	});
+
+
+	//同种图片不同颜色相互切换
+   function changeColorOn(){
+      $(this).children(".first").hide();
+      $(this).children(".next").show();
+      $(this).children(".pro-name").css("color","#32586f");
+   }
+   $(".pro li").bind("mouseover",changeColorOn);
+   $(".pro li").bind("mouseover",changeColorOn);
+   $(".pro li").bind("mouseover",changeColorOn);
+   $(".pro li").bind("mouseover",changeColorOn);
+   $(".pro li").bind("mouseover",changeColorOn);
+   $(".pro li").bind("mouseover",changeColorOn);
+   $(".pro li").bind("mouseout",function(){
+      $(this).children(".first").show();
+      $(this).children(".next").hide();
+      $(this).children(".pro-name").css("color","#d1d1d1");
+   });
+
+
+
 });
